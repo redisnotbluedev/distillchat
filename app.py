ce51594a44d08e9c986c84673c4a35631d20375d
redisnotbluedev <red@redisnot.blue>
2026-03-01 11:34:16 +1030

    html boilerplate

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+load_dotenv()
+BRAND_NAME = os.getenv("BRAND_NAME", "My App")
 
 app = FastAPI(
-	title="app", # Change this when you think of a name!!!
+	title=BRAND_NAME,
 	docs_url=None,
 	redoc_url=None,
 	openapi_url=None
 )
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def root(request: Request):
+	return templates.TemplateResponse(
+		request=request,
+		name="new_chat.html",
+		context={"BRAND_NAME": BRAND_NAME}
+	)
