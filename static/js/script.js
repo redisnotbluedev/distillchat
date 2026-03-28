26c7b7849876b59f781fe67abae0947216c2e311
redisnotbluedev <ededmaude@gmail.com>
2026-03-28 13:39:51 +1030

    fixes

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,15 @@
+/*
+SPDX-License-Identifier: AGPL-3.0-or-later
+Copyright (C) 2026 redisnotblue <147359873+redisnotbluedev@users.noreply.github.com>
+*/
+
 import { state } from "./state.js";
 import "./attachments.js"
 import "./model.js"
 import "./chat.js"
 import { initMessages } from "./messages.js";
 
-const messageContainer = document.getElementById("messages");
-state.currentLeaf = messageContainer?.lastElementChild;
+state.currentLeaf = document.querySelector("#messages > div:last-of-type");
 state.currentModel = localStorage.getItem("model") || document.querySelector("#modelMenu button.selected").dataset.id;
 
 if (!isNewChat) {
