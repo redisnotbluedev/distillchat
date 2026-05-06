7be21594d263576130b26d305b6422627d4027fa
redisnotbluedev <red@redisnot.blue>
2026-05-06 17:09:21 +0930

    i forgot to put licensing on tools

--- tools.py
+++ tools.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2026 redisnotblue <147359873+redisnotbluedev@users.noreply.github.com>
+
 import ai, requests, functools, inspect, time, asyncio, yaml, secrets, tempfile
 from typing import Annotated
 from pathlib import Path
